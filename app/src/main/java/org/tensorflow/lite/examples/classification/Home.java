0c081fd6cb875f4cb438b5e53b4b89f556bcdf56
sachinsav <sachin.saw.13@gmai.com>
2021-05-01 13:57:35 +0530

    api client added

--- app/src/main/java/org/tensorflow/lite/examples/classification/Home.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/Home.java
@@ -1,11 +1,24 @@
 package org.tensorflow.lite.examples.classification;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Home extends AppCompatActivity {
 
@@ -19,6 +32,7 @@ public class Home extends AppCompatActivity {
         profile_btn = (Button)findViewById(R.id.profile_btn);
         goal_btn = findViewById(R.id.goal_btn);
         record_btn = findViewById(R.id.record_btn);
+
         record_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +52,7 @@ public class Home extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Home.this,Today.class);
                 startActivity(intent);
+
             }
         });
         goal_btn.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +61,46 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(Home.this,Goal.class));
             }
         });
+    }
+
+    private void apiTesting() {
+        String url = "http://10.0.2.2:8000/";
+        RequestQueue requestQueue;
+        Context context;
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonrequestQueue;
+        jsonrequestQueue = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("TestingApi",response.getString("data"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("TestingApi","Something wrong "+error.toString());
+            }
+        });
+//        jsonrequestQueue.setRetryPolicy(new RetryPolicy() {
+//            @Override
+//            public int getCurrentTimeout() {
+//                return 50000;
+//            }
+//
+//            @Override
+//            public int getCurrentRetryCount() {
+//                return 50000;
+//            }
+//
+//            @Override
+//            public void retry(VolleyError error) throws VolleyError {
+//
+//            }
+//        });
+        requestQueue.add(jsonrequestQueue);
     }
 }
